fdd2adcba393dc78450a12f5b7b16e91c540e2b3
Kalash Jain <kalashjain446@gmail.com>
2023-11-04 15:06:48 +0530

    initial commit , created base of project

---
+++ Backend/Models/chatModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+    chatName: { type: String, trim: false },
+    isGroupChat: { type: Boolean, default: false },
+
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    lastestMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    },
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+},
+    { timestamps: true, }
+
+)
+
+const chatModel = mongoose.model('Chat', chatSchema)
+module.exports = chatModel;
---
+++ Backend/Models/messageModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    content: { type: String, trim: true },
+    chat: {
+        type: mongoose.Schema.Types.ObjectId, ref: "chatModel"
+    }
+}, { timestamps: true });
+
+const messageModel=mongoose.model("Message",messageModel);
+module.exports=messageModel;
