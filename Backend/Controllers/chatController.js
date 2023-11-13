1868b64efba1daf3c9520599756ccd56b4d374f6
Kalash Jain <kalashjain446@gmail.com>
2023-11-13 15:57:58 +0530

    completed chatController

--- Backend/Controllers/chatController.js
+++ Backend/Controllers/chatController.js
@@ -108,26 +108,81 @@ const createGroupChats = asyncHandler(async (req, res) => {
 });
 
 const renameGroup = asyncHandler(async (req, res) => {
-    const { newName, chatID } = req.body;
-    const chatElem = await Chat.findById(chatID)
-    const adminId = chatElem.groupAdmin;
+    const { newName, chatId } = req.body;
+    console.log(chatId);
+    const chatElem = await Chat.findById(chatId)
     if (!chatElem) {
         return res.status(400).send({ message: "Chat not exists" })
     }
+    const adminId = chatElem.groupAdmin;
+    console.log(adminId);
+    console.log(req.id);
     if (adminId != req.id) {
         return res.status(400).send({ message: "Only Admin can rename" })
     }
 
-    const updatedChat = Chat.findByIdAndUpdate({ _id: chatID }, { chatName: newName }, { new: true })
-        .populate("users", '-password')
+    const updatedChat = await Chat.findByIdAndUpdate({ _id: chatId }, { chatName: newName }, { new: true })
+        .populate('users', '-password')
         .populate('groupAdmin', '-password');
-
     if (!updatedChat)
-        return res.status(400).send({ message: "Some error occured" })
+        return res.status(400).send({ message: "Some error occured in rename group" })
     else {
+        console.log(updatedChat);
         res.status(200).json(updatedChat);
     }
 });
 
+const removeFromGroup = asyncHandler(async (req, res) => {
+    const { removeId, chatId } = req.body;
+    const chatElem = await Chat.findById(chatId)
+    if (!chatElem) {
+        return res.status(400).send({ message: "Chat not exists" })
+    }
+    const adminId = chatElem.groupAdmin;
+    console.log(adminId);
+    console.log(req.id);
+    if (adminId != req.id) {
+        return res.status(400).send({ message: "Only Admin can remove" })
+    }
 
-module.exports = { accessChats, fetchChats, createGroupChats, renameGroup };
+    const updatedChat = await Chat.findByIdAndUpdate({ _id: chatId }, { $pull: { users: { $in: [removeId] } } }, { new: true })
+        .populate('users', '-password')
+        .populate('groupAdmin', '-password');
+
+    if (!updatedChat)
+        return res.status(400).send({ message: "Some error occured in remove user" })
+    else {
+        console.log(updatedChat);
+        res.status(200).json(updatedChat);
+    }
+
+
+
+});
+
+const addToGroup = asyncHandler(async (req, res) => {
+    const { addId, chatId } = req.body;
+    const chatElem = await Chat.findById(chatId)
+    if (!chatElem) {
+        return res.status(400).send({ message: "Chat not exists" })
+    }
+    const adminId = chatElem.groupAdmin;
+    console.log(adminId);
+    console.log(req.id);
+    if (adminId != req.id) {
+        return res.status(400).send({ message: "Only Admin can add people" })
+    }
+    const updatedChat = await Chat.findByIdAndUpdate({ _id: chatId }, { $push: { users: addId }  }, { new: true })
+        .populate('users', '-password')
+        .populate('groupAdmin', '-password');
+
+    if (!updatedChat)
+        return res.status(400).send({ message: "Some error occured in add user" })
+    else {
+        console.log(updatedChat);
+        res.status(200).json(updatedChat);
+    }
+
+});
+
+module.exports = { accessChats, fetchChats, createGroupChats, renameGroup, removeFromGroup, addToGroup };
--- Backend/Routes/chatRoute.js
+++ Backend/Routes/chatRoute.js
@@ -2,14 +2,14 @@
 // const {verifyUser} from '
 const express =require('express');
 const {verifyUser} =require('../Middleware/verifyUser');
-const {accessChats ,fetchChats ,createGroupChats ,renameGroup}=require('../Controllers/chatController');
+const {accessChats ,fetchChats ,createGroupChats ,renameGroup ,removeFromGroup , addToGroup}=require('../Controllers/chatController');
 const router=express.Router();
 
 router.post("/",verifyUser,accessChats);
 router.get("/",verifyUser,fetchChats);
 router.post("/createGroup",verifyUser,createGroupChats);
 router.put("/rename",verifyUser,renameGroup);
-// router.put("/groupremove",verifyUser,removeFromGroup); 
-// router.put("/groupadd",verifyUser,addToGroup); 
+router.put("/groupremove",verifyUser,removeFromGroup); 
+router.put("/groupadd",verifyUser,addToGroup); 
 
 module.exports=router;
